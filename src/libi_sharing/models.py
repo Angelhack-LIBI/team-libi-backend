063d66a53e8aac16d393ae65d6a93727a9861b68
yunjey0 <sehajyang@gmail.com>
2020-07-18 03:40:26 +0900

    feat : funding domain 연관 클래스 작성

--- src/libi_sharing/models.py
+++ src/libi_sharing/models.py
@@ -1,14 +1,41 @@
 from django.db import models
 
-from src.libi_account.models import Account
-from src.libi_common.models import BaseModel
+from libi_common.models import BaseModel
 
 
-class Sharing(models.Model, BaseModel):
-    sharing_id = models.IntegerField(primary_key=True, auto_created=True)
-    title = models.CharField(default='NONE', blank=True, max_length=200)
-    description = models.TextField(default='NONE', blank=True)
-    unit_mapping_id = models.ForeignKey(Unit, null=True, on_delete=models.SET_NULL)
-    location_id = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
-    category_id = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
-    created_user_id = models.ForeignKey(Account, null=True, on_delete=models.SET_NULL)
+class Funding(BaseModel):
+    title = models.CharField(blank=False, max_length=200)
+    description = models.TextField(blank=True)
+    unit_mapping_id = models.ForeignKey('FundingOption', null=True, on_delete=models.SET_NULL)
+    section_id = models.ForeignKey('Section', null=True, on_delete=models.SET_NULL)
+    category_id = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+    created_user_id = models.ForeignKey('libi_account.Account', null=True, on_delete=models.SET_NULL)
+    file = models.ForeignKey('File', null=True, on_delete=models.SET_NULL)
+
+
+class Category(BaseModel):
+    title = models.CharField(blank=False, max_length=200)
+    description = models.TextField(blank=True)
+
+
+class FundingOption(BaseModel):
+    description = models.CharField(blank=False, max_length=16)
+    minimum_price = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+
+
+class SectionGroup(BaseModel):
+    name = models.CharField(blank=False, max_length=50)
+
+
+class Section(BaseModel):
+    section_group_id = models.ForeignKey('SectionGroup', null=False, on_delete=models.SET_NULL)
+    name = models.CharField(blank=False, max_length=100)
+
+
+class File(BaseModel):
+    extension = models.CharField(blank=False, max_length=30)
+    file_type = models.CharField(blank=False, max_length=10)
+    name = models.CharField(blank=False, max_length=100)
+    path = models.CharField(blank=False, max_length=200)
+    size = models.IntegerField(default=0)
