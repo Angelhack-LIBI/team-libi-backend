e16449665bdd742b0bfe7e13123c4d9fa5090f6e
yunjey0 <sehajyang@gmail.com>
2020-07-18 02:37:29 +0900

    feat : sharing modeling 작성

--- src/libi_sharing/models.py
+++ src/libi_sharing/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from src.libi_account.models import Account
+from src.libi_common.models import BaseModel
+
+
+class Sharing(models.Model, BaseModel):
+    sharing_id = models.IntegerField(primary_key=True, auto_created=True)
+    title = models.CharField(default='NONE', blank=True, max_length=200)
+    description = models.TextField(default='NONE', blank=True)
+    unit_mapping_id = models.ForeignKey(Unit, null=True, on_delete=models.SET_NULL)
+    location_id = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
+    category_id = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    created_user_id = models.ForeignKey(Account, null=True, on_delete=models.SET_NULL)
