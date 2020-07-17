632f4dd0b322a9ae09e86b36ebf520d26beb0dd2
yunjey0 <sehajyang@gmail.com>
2020-07-18 05:22:54 +0900

    feat : apply, file group 클래스 작성

--- src/libi_sharing/models.py
+++ src/libi_sharing/models.py
@@ -3,14 +3,15 @@ from django.db import models
 from libi_common.models import BaseModel
 
 
-class Funding(BaseModel):
+class Sharing(BaseModel):
     title = models.CharField(blank=False, max_length=200)
     description = models.TextField(blank=True)
-    unit_mapping_id = models.ForeignKey('FundingOption', null=True, on_delete=models.SET_NULL)
+    unit_mapping_id = models.ForeignKey('libi_sharing.models.SharingOption', null=True, on_delete=models.SET_NULL)
     section_id = models.ForeignKey('Section', null=True, on_delete=models.SET_NULL)
     category_id = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
     created_user_id = models.ForeignKey('libi_account.Account', null=True, on_delete=models.SET_NULL)
-    file = models.ForeignKey('File', null=True, on_delete=models.SET_NULL)
+    price = models.IntegerField(default=0)
+    sharing_type = models.IntegerField(blank=False, null=False)
 
 
 class Category(BaseModel):
@@ -18,7 +19,7 @@ class Category(BaseModel):
     description = models.TextField(blank=True)
 
 
-class FundingOption(BaseModel):
+class SharingOption(BaseModel):
     description = models.CharField(blank=False, max_length=16)
     minimum_price = models.IntegerField(default=0)
     price = models.IntegerField(default=0)
@@ -33,9 +34,26 @@ class Section(BaseModel):
     name = models.CharField(blank=False, max_length=100)
 
 
+class FIleGroup(BaseModel):
+    funding_id = models.ForeignKey('Sharing', null=True, on_delete=models.SET_NULL)
+    file_id = models.ForeignKey('File', null=True, on_delete=models.SET_NULL)
+
+
 class File(BaseModel):
     extension = models.CharField(blank=False, max_length=30)
     file_type = models.CharField(blank=False, max_length=10)
     name = models.CharField(blank=False, max_length=100)
     path = models.CharField(blank=False, max_length=200)
     size = models.IntegerField(default=0)
+
+
+class ApplyGroup(BaseModel):
+    funding_id = models.ForeignKey('Sharing', null=True, on_delete=models.SET_NULL)
+    apply_id = models.ForeignKey('Apply', null=True, on_delete=models.SET_NULL)
+
+
+class Apply(BaseModel):
+    user_id = models.ForeignKey('libi_account.Account', null=True, on_delete=models.SET_NULL)
+    funding_count = models.IntegerField(default=0)
+    funding_price = models.IntegerField(default=0)
+    state = models.CharField(blank=False, max_length=10)
