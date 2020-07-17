2aeb794afb831bfbc5555b3c91bcc92ca8e3a02b
yunjey0 <sehajyang@gmail.com>
2020-07-18 06:50:48 +0900

    fix : blank False 설정 삭제

--- src/libi_sharing/models.py
+++ src/libi_sharing/models.py
@@ -4,7 +4,7 @@ from libi_common.models import BaseModel
 
 
 class Sharing(BaseModel):
-    title = models.CharField(blank=False, max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     section = models.ForeignKey('Section', null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
@@ -14,24 +14,24 @@ class Sharing(BaseModel):
 
 
 class Category(BaseModel):
-    title = models.CharField(blank=False, max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
 
 
 class SharingOption(BaseModel):
     sharing = models.ForeignKey('Sharing', null=True, on_delete=models.SET_NULL)
-    description = models.CharField(blank=False, max_length=16)
+    description = models.CharField(max_length=16)
     minimum_price = models.IntegerField(default=0)
     price = models.IntegerField()
 
 
 class SectionGroup(BaseModel):
-    name = models.CharField(blank=False, max_length=50)
+    name = models.CharField(max_length=50)
 
 
 class Section(BaseModel):
     section_group_id = models.ForeignKey('SectionGroup', null=False, on_delete=models.SET_NULL)
-    name = models.CharField(blank=False, max_length=100)
+    name = models.CharField(max_length=100)
 
 
 class FileGroup(BaseModel):
@@ -40,10 +40,10 @@ class FileGroup(BaseModel):
 
 
 class File(BaseModel):
-    extension = models.CharField(blank=False, max_length=30)
-    file_type = models.CharField(blank=False, max_length=10)
-    name = models.CharField(blank=False, max_length=100)
-    path = models.CharField(blank=False, max_length=200)
+    extension = models.CharField(max_length=30)
+    file_type = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    path = models.CharField(max_length=200)
     size = models.IntegerField(default=0)
 
 
@@ -56,4 +56,4 @@ class Apply(BaseModel):
     account = models.ForeignKey('libi_account.Account', null=True, on_delete=models.SET_NULL)
     funding_count = models.IntegerField(default=0)
     funding_price = models.IntegerField(default=0)
-    state = models.CharField(blank=False, max_length=10)
+    state = models.CharField(max_length=10)
