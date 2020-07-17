622044c42c8322def36b1b77031318e4f92fef34
kde713 <kde713@gmail.com>
2020-07-18 08:11:36 +0900

    media root, url config

--- src/libi_common/utils.py
+++ src/libi_common/utils.py
@@ -1,4 +1,6 @@
 import datetime
+import os
+import secrets
 
 import pendulum
 from django.conf import settings
@@ -19,3 +21,12 @@ def datetime_to_pendulum(dt: datetime.datetime) -> pendulum.DateTime:
     :return: 변환된 pendulum instance
     """
     return pendulum.instance(dt).in_tz(settings.TIME_ZONE)
+
+
+def generate_upload_path(instance, filename):
+    prefix = getattr(instance, 'FILE_UPLOAD_PATH', 'etc')
+    upload_date = now().to_date_string()
+    generated_filename = secrets.token_urlsafe(nbytes=16)
+    file_ext = os.path.splitext(filename)[1]
+
+    return f"{prefix}/{upload_date}/{generated_filename}{file_ext}"
