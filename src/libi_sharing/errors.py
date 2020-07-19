3edc546a51307bb5b2272d1496edb293cb5b39fe
2minchul <2_minchul@naver.com>
2020-07-19 09:54:57 +0900

    fix: NotExistSharingError 를 404로 바꿈

--- src/libi_sharing/errors.py
+++ src/libi_sharing/errors.py
@@ -6,7 +6,7 @@ class NotExistSharingError(APIException):
     """
     sharing 없음 오류
     """
-    status_code = status.HTTP_400_BAD_REQUEST
+    status_code = status.HTTP_404_NOT_FOUND
     default_detail = 'sharing id 가 존재하지 않습니다'
     default_code = 'not_exist'
 
